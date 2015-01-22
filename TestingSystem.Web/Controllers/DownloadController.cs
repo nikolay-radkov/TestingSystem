477c3acd744c647e5174517a313328e0ad29ada4
nikolay-radkov <nikolay.kirilov.radkov@gmail.com>
2015-01-22 13:12:27 +0200

    Added downloaded page for mobile apps

---
+++ TestingSystem.Web/Controllers/DownloadController.cs
@@ -0,0 +1,13 @@
+﻿namespace TestingSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class DownloadController : Controller
+    {
+        // GET: Download
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
